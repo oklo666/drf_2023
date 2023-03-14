1439e8462306186533ba261e80d2f05ad2ef037e
Oklo <oklo@ubuntu.myguest.virtualbox.org>
2023-03-14 21:40:34 +0300

    1

---
+++ library/authors/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Author
+
+class AuthorModelSerializer(ModelSerializer):
+
+
+    class Meta:
+        model = Author
+        fields = '__all__'
